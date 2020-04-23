d357462df9ac1e0086be8fd80669079c13f199fb
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-04-23 19:34:53 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
-  
+  @@all = []
+  @@waiters = []
+
+  attr_accessor :name, :age
+
+   def initialize(name, age)
+    @name = name
+    @age = age
+    Customer.all << self
+   end
+
+    def self.all
+      @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+      Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+      Meal.all.select{|meal| meal.customer == self}
+    end
+
+    def waiters
+       Meal.all.select{|meal| meal.customer == self}.collect{|m| m.waiter}
+     end
 end
