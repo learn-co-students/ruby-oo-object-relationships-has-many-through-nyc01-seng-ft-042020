7efe59727f521eb2b06219efdc73f9f50b58edec
blobbyblobfish <aywang6@gmail.com>
2020-04-23 16:41:25 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
 class Customer
+
+    @@all = []
+  def initialize (name, age)
+    @name = name
+    @age = age
+    self.save
+  end
   
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal (waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals 
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters 
+   meals.map {|meal| meal.waiter}
+  end
+
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,22 @@
 class Meal
+    @@all = []
+
+    attr_accessor :waiter, :customer, :total, :tip
+
+    def initialize (waiter, customer, total, tip)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        self.save
+    end
+
+    def save 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
