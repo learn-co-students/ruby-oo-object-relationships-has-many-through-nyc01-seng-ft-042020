acac81529efea20e1a2965051be622e0ad2d663b
dnamgyal <don.namgyal@hotmail.com>
2020-04-23 21:55:03 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,39 @@
+require 'pry'
 class Waiter
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|person| person.waiter == self}
+    end
+
+
+    def best_tipper
+        best_tip = 0
+        meals.each do |order|
+            if order.tip > best_tip
+                best_tip = order.tip
+            end
+        end
+        order = meals.find {|order| order.tip == best_tip}
+        order.customer 
+    end
+
+
 
 end
