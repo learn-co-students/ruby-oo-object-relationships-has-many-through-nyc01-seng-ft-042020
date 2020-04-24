c7aa974bd2a9f923566544ec98b8b1fccd705b4c
rwilt <rosie.wilt@gmail.com>
2020-04-23 20:42:37 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,44 +1,50 @@
 class Customer
-  attr_accessor :name, :age
-
-  @@all = [] 
-
-  # # ^^ class variable that stores every 
-  #`customer` instance upon initialization.
-
-  def initialize(name,age)
-    @name = name
-    @age = age
-    Customer.all << self 
-  end
-
-  def self.all
-    @@all
-  end
-
-def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
-#notice that we are not using a customer
-# parameter when setting up new_mean. 
-#its because we are the customer.
-# so we only need to use self. 
-end
-
-def meals 
-    Meal.all.select do |meal|
-        meal.customer == self
+    attr_accessor :name, :age
+  
+    @@all = []
+  
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
     end
-end
+  
+    def self.all
+      @@all
+    end
+  
+    def meals
+      Meal.all.select do |meal|
+        meal.customer == self
+      end
+    end
+  
+    def waiters
+      meals.map do |meal|
+        meal.waiter
+      end
+    end
 
-def waiters
-    meals.map.do |meal|
-    meal.waiter
-end
-end
-# ^^ we are using the meals method
-# we just built, to loop over the 
-# array of meals that are linked to this 
-# particular customer. and return the waiters
-# associated w them
-end 
-
+   
+    def new_meal(waiter, total, tip=0)
+      Meal.new(waiter, self, total, tip)
+    end
+  
+    def new_meal_20_percent(waiter, total)
+      tip = total * 0.2
+      Meal.new(waiter, self, total, tip)
+    end
+  
+    def self.oldest_customer
+      oldest_age = 0
+      oldest_customer = nil
+      self.all.each do |customer|
+        if customer.age > oldest_age
+          oldest_age = customer.age
+          oldest_customer = customer
+        end
+      end
+      oldest_customer
+    end
+  
+  end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,39 +1,35 @@
 class Waiter
-attr_accessor :name, :years_experience
 
-@@all = []
-
-# ^^ class variable that stores every 
-#`waiter` instance upon initialization.
-
-def initialize 
-@name = name
-@yrs_experience = yrs_experience
-Waiter.all << self
-end
-
-def self.all
-    @@all
-end
-
-def new_meal(customer, total, tip=0)
-Meal.new(self, customer, total, tip)
-end
-
-def meals
-    Meal.all.select do |meal|
-        meal.waiter == self
-        #does the waiter match to the instance 
-        #of waiter im in? 
+    attr_accessor :name, :yrs_experience
+  
+    @@all = []
+  
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
     end
-end 
-
-def best_tipper
-    best_tipped_meal = meals.max do |meal_a, meal_b|
-        meal_a.tip <=> meal_b.tip
+  
+    def self.all
+      @@all
     end
-    best_tipped_meal.customer
-end
 
+    def meals
+        Meal.all.select do |meal|
+          meal.waiter == self #checking for waiter now
+        end
+      end
 
-end
+      def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+      end
+
+      def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end
+      
+        best_tipped_meal.customer
+      end
+  
+  end
